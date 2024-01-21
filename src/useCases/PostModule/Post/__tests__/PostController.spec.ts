60bdcd1d5dbc1dbe4e76818792e11c3fe156e3c8
Joao Paulo Furtado <joaopaulofurtado@live.com>
2024-01-20 16:23:56 -0800

    Refactor PostController tests to use "it" instead of "test"

--- src/useCases/PostModule/Post/__tests__/PostController.spec.ts
+++ src/useCases/PostModule/Post/__tests__/PostController.spec.ts
@@ -1,10 +1,10 @@
 import { appEnv } from "@constants/appEnv";
 import axios from "axios";
-import { describe, expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { createPostMock, readAllPostsMock } from "./PostControllerMocks";
 
 describe("PostController", () => {
-  test("fetch available users", async () => {
+  it("fetch available users", async () => {
     console.log("appEnv.general.apiURL", appEnv.general.apiURL);
 
     const response = await axios.get(`${appEnv.general.apiURL}/posts`);
@@ -14,7 +14,7 @@ describe("PostController", () => {
     expect(posts).toStrictEqual(readAllPostsMock);
   });
 
-  test("should properly create a new user", async () => {
+  it("should properly create a new user", async () => {
     const response = await axios.post(`${appEnv.general.apiURL}/posts`, createPostMock);
 
     const newPost = response.data;
